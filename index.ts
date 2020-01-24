5a70f6c866c454e391dcb6bd8991c31542830152
Daniel Emery <daniel@sine.co>
2020-01-24 16:25:56 +1030

    Updated to resolve after a duration

--- index.ts
+++ index.ts
@@ -1,15 +1,25 @@
 interface ITestPromiseOptions {
   promise: Promise<any>;
-  resolve: (result?: any) => void;
-  reject: (error?: any) => void;
+  resolve: (result?: any, duration?: number) => void;
+  reject: (error?: any, duration?: number) => void;
 }
 
 export function createTestPromise(): ITestPromiseOptions {
-  let externallyResolve: (result?: any) => void = () => undefined;
-  let externallyReject: (error?: any) => void = () => undefined;
+  let externallyResolve: (result?: any, duration?: number) => void = () =>
+    undefined;
+  let externallyReject: (error?: any, duration?: number) => void = () =>
+    undefined;
   const promise = new Promise((resolve, reject) => {
-    externallyResolve = resolve;
-    externallyReject = reject;
+    externallyResolve = (result, duration = 0) => {
+      setTimeout(() => {
+        resolve(result);
+      }, duration);
+    };
+    externallyReject = (error, duration = 0) => {
+      setTimeout(() => {
+        reject(error);
+      }, duration);
+    };
   });
   return {
     promise,
@@ -18,7 +28,7 @@ export function createTestPromise(): ITestPromiseOptions {
   };
 }
 
-export function createTestPromises(num: number) {
+export function createTestPromises(num: number): ITestPromiseOptions[] {
   return [...Array(num).keys()].map(() => {
     const { resolve, reject, promise } = createTestPromise();
     return { resolve, reject, promise };
